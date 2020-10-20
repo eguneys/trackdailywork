d745dfcf018db8931426193f977205c8db4bbdf1
Emre Guneyler <eguneys@hotmail.com>
2020-10-20 17:56:08 +0000

    removed public/css

--- html/help.js
+++ html/help.js
@@ -9,6 +9,8 @@ Continue from black's move <3... d6 4. a3 a6> `),
     tags.p(`To embed a chess board use ply number like:`),
     tags.p(`=8`),
     tags.br(),
+    tags.p(`Embed variation lines <main2 main 3. d4 d5 4. a3 a6> . "main2" is variation name and "main" is the parent line.`),
+    tags.p(`Make sure to put a space at beginning and end of lines <main3 main 3. d4> .`),
     tags.p(`To publish your article click Review first, then publish.`),
     tags.p(`Happy blogging 📘`)
   ]);
